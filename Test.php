aab9fb8d5a59f356d9c532800a6c102b62c9fea5
Cédric Noël <noel.cedric@live.fr>
2020-08-01 19:07:50 +0200

    Nettoyage, documentation et refactoring

--- Test.php
+++ Test.php
@@ -2,38 +2,81 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
  
-
 use RenaultZoeApi\Giya;
 use RenaultZoeApi\Kamereon;
 
 
-$strFile = dirname(__FILE__) . '/data/credentials.json';
-$arrTokens = json_decode(file_get_contents($strFile), true);
-
-// TODO SI NO TOKEN => LOGIN, SINON CHECK+REFRESH DATAS
-//$res = Giya::login('my_renault_login','my_renault_password');
-//Kamereon::getAccounts($res);
-
-// TODO CHECK TOKEN VALIDITY OR REFRESH        ;
-$objDate = new \DateTime($arrTokens['GiyaIdTokenTime']);    
-$dateNow = new \DateTime("now");
+// Run example
+run();
 
 
-$objDate->add(new \DateInterval('PT900S'));
-if($objDate < $dateNow) {
-    // Refresh token
+/**
+ * Run Example function
+ * To avoid to login to API each call, we save tokens in credentials.json
+ *
+ * @return void
+ */
+function run() {
+    say('> Run started');
+    $arrTokens = []; // Will contains token from credentials.json
+    $strFile = dirname(__FILE__) . '/data/credentials.json';
+    if(file_exists($strFile)) {
+        say('> credentias.json exists, reading it');
+        $arrTokens = json_decode(file_get_contents($strFile), true);
+        // Check token validity
+        checkTokens($arrTokens);
+    } else {
+        say('> credentials.json not found, first login');
+        $arrGiyaTokens = Giya::login('my_renault_login','my_renault_password');
+        // get AccountId and save tokens in credentials.json file
+        $arrTokens = Kamereon::getAccounts($arrGiyaTokens);
+        Kamereon::getVehicles($arrTokens); // You can get VIN here
+    }
+      
+    // Example via Kamereon class
+    say('> getBattery via Kamereon class');
+    $strJson = Kamereon::getBattery('my_vin', $arrTokens);
+    say($strJson);
+
+    say('> getCockpit via Kamereon class');
+    $strJson = Kamereon::getCockpit('my_vin', $arrTokens); 
+    say($strJson);
     
-    echo "need refresh\n\r";
-    //log::add('zoe', 'debug','Need tokens refresh');
-    
-    $arrGiyaIdToken = Giya::getJwtToken($arrTokens['GiyaToken'],'','');
-    $arrTokens['GiyaIdTokenTime'] = $arrGiyaIdToken['GiyaIdTokenTime'];
-    $arrTokens['GiyaIdToken'] = $arrGiyaIdToken['GiyaIdToken'];
-
-    $strAccessToken = Kamereon::getToken($arrTokens['accountId'],$arrTokens, true);
-    $arrTokens['kamereon-authorization'] = $strAccessToken;
-    
+    // Example via Vehicle class
+    // TODO
 }
 
-Kamereon::getBattery('VIN', $arrTokens);
-Kamereon::getCockpit('VIN', $arrTokens); 
+/**
+ * Check token validity and update them
+ *
+ * @param array $arrTokens
+ * @return void
+ */
+function checkTokens(&$arrTokens) {
+    $objDate = new \DateTime($arrTokens['GiyaIdTokenTime']);    
+    $objDateNow = new \DateTime("now");
+
+    $objDate->add(new \DateInterval('PT900S'));
+    if($objDate < $objDateNow) {
+        // Refresh token        
+        say('> Tokens needs refresh');
+        // Refresh tokens 
+        $arrGiyaIdToken = Giya::getJwtToken($arrTokens['GiyaToken'],'','');
+        $arrTokens['GiyaIdTokenTime'] = $arrGiyaIdToken['GiyaIdTokenTime'];
+        $arrTokens['GiyaIdToken'] = $arrGiyaIdToken['GiyaIdToken'];
+
+        Kamereon::saveTokens($arrTokens);
+        // From an update of Renault API in june 2020, kamereon-authorization token is no more required        
+    }
+}
+
+/**
+ * Makes echo !
+ *
+ * @param string $strMessage
+ * @return void
+ */
+function say($strMessage) {
+    echo $strMessage."\n\r";
+}
+
